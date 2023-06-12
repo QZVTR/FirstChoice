be6b6b1bc3f4830fc87eb5d2e7640bafaf567710
Edward Jermyn <85450094+QZVTR@users.noreply.github.com>
2023-06-12 01:35:57 +0100

    Delete Source directory

--- Source/pages/components/landingPage/AboutUs.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function AboutUs() {
-  return (
-    <>
-        <h3><u>About us</u></h3>
-    </>
-  )
-}
--- Source/pages/components/landingPage/HowWeWork.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function HowWeWork() {
-  return (
-    <>
-        <h3><u>How we work</u></h3>
-    </>
-  )
-}

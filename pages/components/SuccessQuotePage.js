182ff59df5aea422188105a5e7872f45e6867a01
Edward <edwardjermyn52@gmail.com>
2023-06-14 21:57:33 +0100

    Success page for quotes

---
+++ pages/components/SuccessQuotePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Layout from '../Layout'
+export default function SuccessQuotePage() {
+  return (
+    <Layout>
+    <div>Your request for a quote was successfull!</div>
+    </Layout>
+  )
+}

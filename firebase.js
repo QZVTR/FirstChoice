b1fc03720c0cdce0300e70bb2b93ab3ce477a995
Edward <edwardjermyn52@gmail.com>
2023-06-22 22:11:09 +0100

    Switched firebase file to use environment variables for safety, also uploading faq page still working on stripe

--- firebase.js
+++ firebase.js
@@ -9,13 +9,14 @@ import { getStorage } from 'firebase/storage'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBiiwnP5rFL8nz2nUr5ykDPkxnbFkD8K40",
-  authDomain: "firstchoice-bc61b.firebaseapp.com",
-  projectId: "firstchoice-bc61b",
-  storageBucket: "firstchoice-bc61b.appspot.com",
-  messagingSenderId: "730421440295",
-  appId: "1:730421440295:web:8d4edffe71ca945a324302"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOM,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
--- pages/settings/FAQ.js
+++ pages/settings/FAQ.js
@@ -4,6 +4,60 @@ export default function FAQ() {
   return (
     <Layout>
       <div>FAQ</div>
+      <div>
+      <div>
+        <h4>How does First Choice work? </h4>
+        <div>Tradespeople can create profiles on our platform showcasing what services they can provide. Customers can browse through these profiles to find suitable tradespeople for their specific needs. Our platform acts as a bridge, facilitating communication and transactions between tradespeople and customers.</div>
+      </div>
+      <div>
+        <h4>What types of tradespeople are featured on First Choice?</h4>
+        <div>
+        We welcome a wide range of tradespeople on our platform. This includes professionals such as plumbers, electricians, carpenters, painters, landscapers, roofers, and more we can add what other services. We strive to cover a comprehensive range of trades to meet the diverse needs of our users. 
+        </div>
+      </div>
+      <div>
+        <h4>How can tradespeople advertise their services on First Choice?</h4>
+        <div>
+        Tradespeople can sign up on our platform and create a detailed profile. They can highlight their skills, years of work and qualifications They can also upload photos or videos showcasing their past projects to provide potential customers with a glimpse of their workmanship. 
+        </div>
+      </div>
+      <div>
+        <h4>How can customers find tradespeople on First Choice?</h4>
+        <div>
+        Customers can search for tradespeople based on their location, the type of service they require, and other specific criteria. They can browse through profiles, compare tradespeople, read reviews from previous customers, and directly contact tradespeople to discuss their project requirements or request quotes. 
+        </div>
+      </div>
+      <div>
+        <h4>Are the tradespeople on First Choice vetted?</h4>
+        <div>
+        While we do not personally vet every tradesperson on our platform, we provide a system for customers to leave reviews and ratings based on their experience. This feedback helps future customers make informed decisions when choosing a tradesperson. We encourage customers to thoroughly evaluate tradespeople before hiring them and to ask for references or proof of qualifications if desired. 
+        </div>
+      </div>
+      <div>
+        <h4>Can customers make payments through First Choice?</h4>
+        <div>
+        Currently, we facilitate the initial connection between customers and tradespeople. However, the actual payment and financial transactions are handled directly between the two parties. We encourage both parties to discuss and agree upon payment terms in advance to ensure transparency and avoid any misunderstandings.
+        </div>
+      </div>
+      <div>
+        <h4>What if a customer is dissatisfied with a tradesperson's work?</h4>
+        <div>
+        While we cannot directly intervene in disputes between customers and tradespeople, we take feedback and reviews seriously. If a customer has a negative experience with a tradesperson, we encourage them to leave an honest review detailing their concerns. This helps other customers make informed decisions, and it also allows us to monitor the quality of services being offered. 
+        </div>
+      </div>
+      <div>
+        <h4>How can users contact our customer support?</h4>
+        <div>
+        If you have any further questions or require assistance, you can reach our customer support team through the "Contact Us" section on our website. We strive to respond to inquiries promptly and provide the necessary support to both tradespeople and customers using our platform.
+        </div>
+      </div>
+      <div>
+        <h4>Is there a fee for tradespeople to advertise on your platform?</h4>
+        <div>
+        Yes, we offer different subscription packages for tradespeople to choose from, they range from monthly and annually. With the annual package, you can get one month free! The subscription fees help us maintain and enhance the platform's functionality.
+        </div>
+      </div>
+      </div>
     </Layout>
   )
 }
